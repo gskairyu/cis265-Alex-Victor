8b73d651719707214ff53dcff1ebe7d07462dd18
gskairyu <36314268+gskairyu@users.noreply.github.com>
2018-02-25 11:48:46 -0500

    Update PalindromeRecursive.java

--- assignment/PalindromeRecursive.java
+++ assignment/PalindromeRecursive.java
@@ -8,20 +8,31 @@
 		}}
 	*/
 import java.util.Scanner; 
+
 public class PalindromeRecursive { 
+
 	  public static void main(String[] args) 
 	  { 
-		  System.out.print("Enter a string to check if it is palindrome :: "); 
+		  System.out.print("Enter a string to check if it is palindrome :: ");
+ 
 		  Scanner input = new Scanner(System.in); 
-		  String inputString = input.nextLine(); 
+
+		  String inputString = input.nextLine();
+ 
 		  String palindromeString = cleanupString(inputString); 
+
 		  boolean isPalindrome = checkPalindrome(palindromeString, 0, palindromeString.length()-1);
-		  System.out.println(isPalindrome); 
+
 		  if (isPalindrome) { 
+
 		   System.out.println("The given string is a palindrome"); 
+
 		  } 
+
 		  else { 
+
 		   System.out.println("The given string is not a palindrome"); 
+
 		  }
 		  
 		  input.close(); 
@@ -31,36 +42,31 @@ public class PalindromeRecursive {
  /* Creating a method for performing string clean operation to get rid of unwanted characters */ 
 	   private static String cleanupString(String s) 
 	   {
-	       String cleanupStr = s.replaceAll("[\\W]", "");   
-
+	       // replaceAll removes all special characters and spaces.
+	       String cleanupStr = s.replaceAll("[\\W]", "");
+	       // toLowerCase used to make the string a lowercase value.
 	       cleanupStr = cleanupStr.toLowerCase();
-
-	       System.out.println(cleanupStr);
 	      
 	       return cleanupStr; 
 	    } 
-	   
+
+
 	   
  	   private static boolean checkPalindrome(String inputString, int low, int high) { 
-
-               boolean tcheck = false;
-	       if (low <= high){
-		   StringBuilder inputstring2=new StringBuilder();
-	 	   inputstring2.append(inputString);
-	 	   String check=inputstring2.toString();
-	 	   System.out.println(check);//debug track
-	 	   char highChar = check.charAt(high);
-	 	   char lowChar = check.charAt(low);
-	 	   System.out.println("second mark");//  debug track
-	 	   if (highChar == lowChar){
-	     		tcheck = true;
-		   }
-		 }
-	       else {
-		checkPalindrome(inputString,(low + 1),(high - 1));
-	       }
-	       return tcheck;
-	 
+		
+		// Takes the character at the last position and sets it to a variable.
+	        char highChar = inputString.charAt(high);
+		// Takes the character at the first position and sets it to a variable.
+	        char lowChar = inputString.charAt(low);
+		// Checks to see if the string has recursively gone through all possible characters and if so, returns true.
+	        if (low > high){
+	           return true;
+                }
+		// Recursively calls the method to check the next lowest if they are equivalent.
+	        if (highChar == lowChar){
+	           return checkPalindrome(inputString,(low + 1),(high - 1));
+	        }
+		// Returns false when they are not equivilant and the value of low does not exceed the value of high.	 
+	        return false;
  } 
 }  
-
