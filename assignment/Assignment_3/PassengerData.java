44ca6532ac82f44487dcbd2399d94f62a74df160
cerealdrop <raphdv15@gmail.com>
2018-03-25 21:55:27 -0400

    if you look at the ouput everything works except for the numbers, are you still up to fix that

--- assignment/Assignment_3/PassengerData.java
+++ assignment/Assignment_3/PassengerData.java
@@ -18,20 +18,11 @@ class PassengerData extends Passenger {
     	Passenger gat=new Passenger(); 
     	System.out.println("Enter Passenger Name");
     	gat.setName((input.nextLine()).toUpperCase());
-    	System.out.println("Enter the ticket type(AC/Gen");
+    	System.out.println("Enter the ticket type(AC/Gen)");
     	gat.setTicket_type((input.nextLine()).toUpperCase());
     	System.out.println("Enter the Seat number");
     	gat.setSeat_no(input.nextInt());
-    	createQueue.enqueue(gat);
-    	System.out.println(gat); 
-    
-   	 
-    	
-    	/*System.out.println("Enter the ticket type(AC/Gen");
-    	setTicket_type((input.nextLine()).toUpperCase());
-    	System.out.println("Enter the Seat number");
-    	setSeat_no(input.nextInt());*/
-    	
+    	createQueue.enqueue(gat);   	
     	
      break; 
     case 'R': 
@@ -41,27 +32,37 @@ class PassengerData extends Passenger {
      break; 
     case 'S': 
     //SCAN AND DISPLAY DETAILS OF A PARTICULAR PASSENGER
+    	if (createQueue.hasItems()==false) {
+    		
+    		System.out.println("The list is empty"); 
+    		break;
+    	}
+    	else {
     	System.out.println("Enter the passenger whose details are neeeded");
     	String check=(input.nextLine()).toUpperCase();
+    	String keep=""; 
     	Iterator k=createQueue.iterator() ;
+    	while(k.hasNext()){
     	String gat2=String.valueOf(k.next());
-    	System.out.println(gat2);
-    	String[] splitStr = gat2.split("\\s+");
-    	System.out.println(splitStr[0]);
-    	//while(k.hasNext()){
-        	//String[] splitStr = gat2.split("\\s+");
-    	/*if(splitStr[0].equals(check)) {
-    		
-    		System.out.println(k.next());
-    		break;
-    	}*/
-    	//}
-    	//System.out.println(k.next());
-    		System.out.println("Passsenger with the name: "+check +"not found");
-    		//System.out.println(splitStr[0]);
+    	String[] splitStr = gat2.split("\\s");
     	
+    	if(splitStr[0].equals(check)) {
+    		keep=splitStr[0];
+    		System.out.println(gat2);
+    		break;
+    	}
+    	}
+    	
+    	if(keep.equals(check)) {	
+    	break;
+    	}
+    	else {
+    	 System.out.println("Passsenger with the name: "+check +"not found");
+    	 break;
+    	}
+    	}	
      /* YOUR CODE HERE */ 
-     break; 
+     
     case 'Q': 
     //DISPLAY CONTENTS IN QUEUE AND QUIT THE LOOP
     	Iterator g=createQueue.iterator() ;
